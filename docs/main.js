e38206f3e242ebed0c0aec112de0054691da7c5f
basashi-G <basashi_tech@icloud.com>
2023-10-25 14:38:50 +0000

    Pending changes exported from your codespace

---
+++ docs/main.js
@@ -0,0 +1,10 @@
+let url = new URL(window.location.href);
+let params = url.searchParams;
+if (params.get('to') ===  null){
+    window.location.href="https://github.com/basashi-G/redirect";
+}else{
+    window.location.href=decodeURIComponent(params.get('to'));
+}
+
+
+
--- main.js
+++
@@ -1,5 +0,0 @@
-let url = new URL(window.location.href);
-let params = url.searchParams;
-window.location.href=decodeURIComponent(params.get('to'));
-
-
